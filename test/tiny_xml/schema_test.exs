4f31be9835aef707a647dee9915eb4930bcdff77
Samuele Battistella <samuele.battistella@athonet.com>
2021-10-01 17:04:38 +0200

    Improve test

--- test/tiny_xml/schema_test.exs
+++ test/tiny_xml/schema_test.exs
@@ -1,16 +1,21 @@
 defmodule SchemaTest do
   use ExUnit.Case, async: true
 
-  import Test.Data, only: [dummy_xml: 0]
+  @dummy_data_path "test/data/dummy_data.xml"
+  @schema_path "test/data/dummy_schema.xsd"
 
-  @schema_path "test/tiny_xml/dummy_schema.xsd"
+  setup_all do
+    dummy_xml = File.read!(@dummy_data_path)
 
-  test "xml should be valid" do
-    assert :ok = TinyXml.Schema.validate(dummy_xml(), @schema_path)
+    {:ok, dummy_xml: dummy_xml}
   end
 
-  test "xml should be invalid" do
-    invalid_xml = String.replace(dummy_xml(), "orderperson", "person")
+  test "xml should be valid", %{dummy_xml: dummy_xml} do
+    assert :ok = TinyXml.Schema.validate(dummy_xml, @schema_path)
+  end
+
+  test "xml should be invalid", %{dummy_xml: dummy_xml} do
+    invalid_xml = String.replace(dummy_xml, "orderperson", "person")
 
     assert {:error, _} = TinyXml.Schema.validate(invalid_xml, @schema_path)
   end
--- test/tiny_xml_test.exs
+++ test/tiny_xml_test.exs
@@ -1,35 +1,41 @@
 defmodule TinyXmlTest do
   use ExUnit.Case, async: true
 
-  import Test.Data, only: [dummy_xml: 0]
+  @dummy_data_path "test/data/dummy_data.xml"
 
   doctest TinyXml
 
-  test "should get xml node from xml string" do
-    result = TinyXml.first(dummy_xml(), "/shiporder/orderperson")
+  setup_all do
+    dummy_xml = File.read!(@dummy_data_path)
+
+    {:ok, dummy_xml: dummy_xml}
+  end
+
+  test "should get xml node from xml string", %{dummy_xml: dummy_xml} do
+    result = TinyXml.first(dummy_xml, "/shiporder/orderperson")
 
     assert is_tuple(result)
   end
 
-  test "should get xml node list from xml string" do
-    result = TinyXml.all(dummy_xml(), "/shiporder/item")
+  test "should get xml node list from xml string", %{dummy_xml: dummy_xml} do
+    result = TinyXml.all(dummy_xml, "/shiporder/item")
 
     assert is_list(result)
     assert length(result) == 2
   end
 
-  test "should get xml node value" do
+  test "should get xml node value", %{dummy_xml: dummy_xml} do
     result =
-      dummy_xml()
+      dummy_xml
       |> TinyXml.first("/shiporder/shipto/country")
       |> TinyXml.text()
 
     assert result == "Norway"
   end
 
-  test "should get xml node attribute" do
+  test "should get xml node attribute", %{dummy_xml: dummy_xml} do
     result =
-      dummy_xml()
+      dummy_xml
       |> TinyXml.first("/shiporder")
       |> TinyXml.attribute("orderid")
 
