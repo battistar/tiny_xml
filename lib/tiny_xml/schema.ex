60e3e9bd1f114f3915c89dbb0ce91978997a505b
Samuele Battistella <samuele.battistella@athonet.com>
2021-10-20 23:48:41 +0200

    Improve schema

--- lib/tiny_xml/schema.ex
+++ lib/tiny_xml/schema.ex
@@ -1,8 +1,28 @@
 defmodule TinyXml.Schema do
+  @moduledoc """
+  XML schema utility functions.
+  """
+
   import TinyXml.Utils, only: [from_string: 1]
 
-  @spec validate(binary, binary) :: :ok | {:error, [tuple()]}
-  def validate(xml_string, schema_path) do
+  @doc """
+  Validates the `xml_string` trought the XML schema file at `schema_path`.
+
+  Returns `:ok` if XML is valid otherwise `{:error, reason}`.
+
+    ## Examples
+      xml = "
+        <Person>
+          <Name>Max</Name>
+          <Surname>Power</Surname>
+        </Person>
+      "
+
+      TinyXml.Schema.validate_string(xml, "path/to/schema.xsd")
+      #=> :ok
+  """
+  @spec validate_string(String.t(), Path.t()) :: :ok | {:error, [tuple()]}
+  def validate_string(xml_string, schema_path) do
     xml_data = from_string(xml_string)
     schema_path = to_charlist(schema_path)
 
@@ -11,6 +31,23 @@ defmodule TinyXml.Schema do
     case :xmerl_xsd.validate(xml_data, schema) do
       {:error, reason} -> {:error, reason}
       _ -> :ok
+    end
+  end
+
+  @doc """
+  Validates an XML file at `xml_path` trought the XML schema file at `schema_path`.
+
+  Returns `:ok` if XML is valid otherwise `{:error, reason}`.
+
+    ## Examples
+      TinyXml.Schema.validate_file("path/to/file.xml", "path/to/schema.xsd")
+      #=> :ok
+  """
+  @spec validate_file(Path.t(), Path.t()) :: :ok | {:error, [tuple()]}
+  def validate_file(xml_path, schema_path) do
+    case File.read(xml_path) do
+      {:ok, xml_string} -> validate_string(xml_string, schema_path)
+      err -> err
     end
   end
 end
--- test/tiny_xml/schema_test.exs
+++ test/tiny_xml/schema_test.exs
@@ -1,22 +1,30 @@
 defmodule SchemaTest do
   use ExUnit.Case, async: true
 
-  @dummy_data_path "test/data/dummy_data.xml"
+  @data_path "test/data/dummy_data.xml"
   @schema_path "test/data/dummy_schema.xsd"
 
   setup_all do
-    dummy_xml = File.read!(@dummy_data_path)
+    dummy_xml = File.read!(@data_path)
 
     {:ok, dummy_xml: dummy_xml}
   end
 
-  test "xml should be valid", %{dummy_xml: dummy_xml} do
-    assert :ok = TinyXml.Schema.validate(dummy_xml, @schema_path)
+  test "xml string should be valid", %{dummy_xml: dummy_xml} do
+    assert :ok = TinyXml.Schema.validate_string(dummy_xml, @schema_path)
   end
 
-  test "xml should be invalid", %{dummy_xml: dummy_xml} do
+  test "xml string should be invalid", %{dummy_xml: dummy_xml} do
     invalid_xml = String.replace(dummy_xml, "orderperson", "person")
 
-    assert {:error, _} = TinyXml.Schema.validate(invalid_xml, @schema_path)
+    assert {:error, _} = TinyXml.Schema.validate_string(invalid_xml, @schema_path)
+  end
+
+  test "xml file should be valid" do
+    assert :ok = TinyXml.Schema.validate_file(@data_path, @schema_path)
+  end
+
+  test "xml file should not exists" do
+    assert {:error, _} = TinyXml.Schema.validate_file("invalid/data/path", @schema_path)
   end
 end
