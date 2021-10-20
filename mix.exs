626ca050202577f2b3f666d6a000609c8e06a82a
Samuele Battistella <samuele.battistella@athonet.com>
2021-10-20 22:27:30 +0200

    Add dialyzer cache

--- mix.exs
+++ mix.exs
@@ -9,12 +9,8 @@ defmodule TinyXml.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [
-        coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test
-      ]
+      preferred_cli_env: preferred_cli_env(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -28,6 +24,22 @@ defmodule TinyXml.MixProject do
     [
       {:excoveralls, "~> 0.10", only: :test},
       {:dialyxir, "~> 1.1", only: :dev, runtime: false}
+    ]
+  end
+
+  defp preferred_cli_env do
+    [
+      coveralls: :test,
+      "coveralls.detail": :test,
+      "coveralls.post": :test,
+      "coveralls.html": :test
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_file: {:no_warn, "deps/dialyzer.plt"},
+      plt_add_apps: [:plug]
     ]
   end
 end
