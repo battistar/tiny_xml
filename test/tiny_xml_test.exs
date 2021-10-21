cebf923a40f9cd6dfeaffe32a1084ef1041b020b
Samuele Battistella <samuele.battistella@athonet.com>
2021-10-21 18:28:27 +0200

    Update test

--- test/tiny_xml_test.exs
+++ test/tiny_xml_test.exs
@@ -18,10 +18,19 @@ defmodule TinyXmlTest do
   end
 
   test "should get xml node list from xml string", %{dummy_xml: dummy_xml} do
-    result = TinyXml.all(dummy_xml, "/shiporder/item")
+    xml_nodes = TinyXml.all(dummy_xml, "/shiporder/item")
 
-    assert is_list(result)
-    assert length(result) == 2
+    assert is_list(xml_nodes)
+    assert length(xml_nodes) == 2
+
+    result =
+      Enum.map(xml_nodes, fn xml_node ->
+        xml_node
+        |> TinyXml.first("/item/price")
+        |> TinyXml.text()
+      end)
+
+    assert result == ["10.90", "9.90"]
   end
 
   test "should get xml node value", %{dummy_xml: dummy_xml} do
